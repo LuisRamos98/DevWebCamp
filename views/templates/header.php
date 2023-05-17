d72e83c784653f089009412cc8d1edbe7e7b6153
LuisRamos <luisandres11061998@gmail.com>
2023-05-17 21:11:27 +0200

    Añadiendo los estilos al header

--- views/templates/header.php
+++ views/templates/header.php
@@ -12,7 +12,19 @@
             </a>
             <p class="header__texto">Octubre 5-6-2023</p>
             <p class="header__texto header__texto--modalidad">En Línea - Presencial</p>
-            <a href="/registro">Comprar Pase</a>
+            <a href="/registro" class="header__boton">Comprar Pase</a>
         </div>
     </div>
 </header>
+
+<div class="barra">
+    <div class="barra__contenido">
+        <a href="/"><h2 class="barra__logo">&#60;DevWebCamp/></h2></a>
+        <nav class="navegacion">
+            <a href="/devwebcamp" class="navegacion__enlace">Evento</a>
+            <a href="/paquetes" class="navegacion__enlace">Paquetes</a>
+            <a href="/workshop-conferencias" class="navegacion__enlace">Workshop / Conferencias</a>
+            <a href="/registro" class="navegacion__enlace">Comprar Boleto</a>
+        </nav>
+    </div>
+</div>

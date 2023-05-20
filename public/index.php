fcb06a4c7763fdcae17b089984545270656553fd
LuisRamos <luisandres11061998@gmail.com>
2023-05-20 22:33:47 +0200

    Creando el formulario de registro de ponentes, estilos/funciones

--- public/index.php
+++ public/index.php
@@ -38,6 +38,7 @@ $router->get('/confirmar-cuenta', [AuthController::class, 'confirmar']);
 $router->get('/admin/dashboard', [AdminController::class, 'index']);
 
 $router->get('/admin/ponentes', [PonentesController::class, 'index']);
+$router->get('/admin/ponentes/crear', [PonentesController::class, 'crear']);
 
 $router->get('/admin/eventos', [EventosController::class, 'index']);
 

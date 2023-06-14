e456708f40eb08c2e16d6ae554ec20ba8e1bf150
LuisRamos <luisandres11061998@gmail.com>
2023-06-14 21:55:53 +0200

    Añadiendo logica y estilos al admin index

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -2,14 +2,29 @@
 
 namespace Controllers;
 
+use Model\Registro;
+use Model\Usuario;
 use MVC\Router;
 
 class AdminController {
 
     public static function index(Router $router) {
 
+        $registros = Registro::get(5);
+
+        foreach($registros as $registro) {
+            $registro->usuario = Usuario::find($registro->usuario_id);
+        }
+
+        $virtual = Registro::total('paquete_id', 2);
+        $presencial = Registro::total('paquete_id', 3);
+
+        $ingresos = ($virtual * 46.1) + ($presencial * 189.54);
+
         $router->render('admin/dashboard/index',[
-            'titulo' => 'Panel de Administración'
+            'titulo' => 'Panel de Administración',
+            'registros' => $registros,
+            'ingresos' => $ingresos
         ]);
     }
 }
--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -1,1 +1,17 @@
 <h2 class="dashboard__heading"><?php echo $titulo;?></h2>
+
+<main class="bloques">
+    <div class="bloques__grid">
+        <div class="bloque">
+            <h3 class="bloque__heading">Últimos Registros</h3>
+
+            <?php foreach($registros as $registro):?>
+                <p class="bloque__texto"><?php echo $registro->usuario->nombre . ' ' . $registro->usuario->apellido;?></p>
+            <?php endforeach;?>
+        </div>
+        <div class="bloque">
+            <h3 class="bloque__heading">Ingresos</h3>
+            <p class="bloque__texto bloque__texto--cantidad"><?php echo $ingresos; ?></p>
+        </div>
+    </div>
+</main>
